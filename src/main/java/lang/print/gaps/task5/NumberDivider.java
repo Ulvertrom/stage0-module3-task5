a5268a36da5afe0197382f9360624e053a204284
ulvertrom <ulvertrom@gmail.com>
2023-02-08 13:33:14 +0400

    Modified 2 progs

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -1,10 +1,8 @@
 package lang.print.gaps.task5;
 
 public class NumberDivider {
-    public static void divide(int toBeDivided) {
-        System.out.println(toBeDivided + " / 2 = " + (toBeDivided / 5.0));
+    void divide(int toBeDivided) {
+        System.out.println((toBeDivided / 5.0));
     }
-    public static void main(String[] args) {
-        divide(11);
-    }
+
 }
--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -1,10 +1,8 @@
 package lang.print.gaps.task5;
 
 public class TimeConvertor {
-    public static void convert(float minutes) {
-        System.out.println(minutes + " minutes = " + (minutes*60) + " seconds");
+    public void convert(float minutes) {
+        System.out.println((minutes*60));
     }
-    public static void main(String[] args) {
-        convert(12);
-    }
+
 }
